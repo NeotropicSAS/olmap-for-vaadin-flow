5239684a7769f9e82b61e25500e3c0cc0bad3b3b
Julian Camacho <julian.camacho@kuwaiba.org>
2025-09-30 16:08:52 -0500

    Initial commit: Snapshot of codebase from Subversion [(7629)](https://sourceforge.net/p/kuwaiba/code/7629/tree/components/olmap-flow/branches/version-24.6.2/olmap-flow-root/)

---
+++ olmap-flow-demo/src/main/java/com/neotropic/flow/component/olmap/demo/CustomAppShell.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.neotropic.flow.component.olmap.demo;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.PWA;
+
+@Push
+@PWA(name = "Vaadin Application",
+        shortName = "Vaadin App",
+        description = "This is an example Vaadin application.")
+public class CustomAppShell implements AppShellConfigurator {
+    
+}
